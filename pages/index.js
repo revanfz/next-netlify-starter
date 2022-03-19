b9f5c7db436d6a5a145bdb1b1ee77605781e8eb4
handshakers01 <74522735+handshakers01@users.noreply.github.com>
2022-03-19 16:04:06 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Revan Fauzi ALgifari</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Sm:)le!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Nyoba aja <code>y</code>
         </p>
       </main>
 
