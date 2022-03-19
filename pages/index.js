04a359abbacf5a4c828aca7a03aa5559bd9adbbd
handshakers01 <74522735+handshakers01@users.noreply.github.com>
2022-03-19 16:08:14 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Revan Fauzi ALgifari</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/cowok.png" />
       </Head>
 
       <main>
